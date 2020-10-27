986e970b05a4daf794c3151c3f34e2e589b7aee4
jschiro99 <jschiro99@gmail.com>
2020-10-27 12:31:21 -0400

    Added a nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Navbar.css"
 import profile from "./images/profile_picture.jpg"
 import searchIcon from "./images/searchIcon.png"
-import dropdownJS from "./scripts/dropdown.js"
 
 
 function NavBar() {
@@ -10,27 +9,24 @@ function NavBar() {
 
   return <div className="navbar">
     
-    <img src="logo.jpg" alt="LOGO" width="75"></img>
+    {/*
+    TODO: Once we have a logo, insert into img src
+    <img src="logo.jpg" alt="LOGO" width="75"></img> */}
     
+    <p className="logo">LOGO</p>
+
     <div className="middle">
       <div className="searchBar">
         <img class="searchIcon" src={searchIcon} alt="SrIc" width="20"></img> 
         <input class="searchInput" type="text" placeholder="Search: e.g. Coding Hub" width="500"></input>
       </div>
-      <button className="searchButton" className="submitButton" type="submit">SEARCH</button>
+      <button className="searchButton" className="searchButton" type="submit">SEARCH</button>
     </div>
 
     <div class="profilePicture">
-      <img className="profilePic" src={profile} alt="PROFILEPIC" width="50"></img>
+      <img className="profilePic" src={profile} alt="PROFILEPIC" width="52"></img>
+      <p class="profileName">Joseph Schiro</p>
 
-      <div class="dropdown">
-        <button class="profileName" onclick="myFunction()" class="dropbutton">Joseph Schiro</button>
-        <div id="myDropdown" class="dropdown-content">
-          <a href="#">Profile</a>
-          <a href="#">Friends</a>
-          <a href="#">Settings</a>
-        </div>
-      </div>
     </div>
 
   </div>
