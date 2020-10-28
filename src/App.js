8a1b6a7a064b72a82758ee603dc4c2eaa4337c56
Chris Ovalle <chrisovalle1@gmail.com>
2020-10-28 13:24:27 -0400

    updating branch

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Login from "./routes/Login";
 import About from "./routes/About";
 import Home from "./routes/Home";
-
+import Cards from "./components/Cards";
 //nelson was here
 
 //anthony was here
@@ -21,36 +21,14 @@ import Home from "./routes/Home";
 
 function App() {
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-        </Navbar.Collapse>
-      </Navbar>
-
+    
       <div>
+        
         <Router>
           <Switch>
+          <Route path = "/cards">
+              <Cards/>
+            </Route>
             <Route path="/about">
               <About />
             </Route>
@@ -60,11 +38,16 @@ function App() {
             <Route path="/">
               <Home />
             </Route>
+            
+
+
           </Switch>
         </Router>
       </div>
-    </div>
   );
 }
 
 export default App;
+
+
+
