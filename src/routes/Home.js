12f8df1a571f9a40284fc3ee6979dbdc395bc826
Chris Ovalle <chrisovalle1@gmail.com>
2020-10-29 19:55:27 -0400

    added card to home

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,25 +1,12 @@
 import React from "react";
 import { Button, Form, FormControl } from "react-bootstrap";
+import Cards from "../components/Card/Cards.jsx";
+
+
 
 function Home() {
   return <div> 
-    <Form>
-      <Form.Group contolId= "formBasicEmail">
-        <Form.Label>Email Address</Form.Label>
-        <Form.Control type = "email" placeholder= "Enter Email" />
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant= "primary" type="submit">
-        Submit
-      </Button>
-  </Form>
+   <Cards/>
   </div>;
 
 
